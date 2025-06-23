2406be671fea96d00a00f85e98597d743b242e67
MinKyung Park <pmk10077@gmail.com>
2025-06-24 08:23:48 +0900

    news

---
+++ main.js
@@ -0,0 +1,46 @@
+
+let newsList = [];
+const menus = document.querySelectorAll(".menus button");
+menus.forEach(menus=>menus.addEventListener("click",(event)=>getNewsByCategory(event)))
+
+const getLastestNews = async () => {
+  const url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${APIKey}`
+  );
+  const response = await fetch(url) //인터넷 접속해서 접근
+  const data = await response.json();
+  newsList = data.articles;
+  render();
+};
+
+const getNewsByCategory = async (event) => {
+  const category = event.target.textContent.toLowerCase();
+  
+  const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${APIKey}`)
+  const response = await fetch(url);
+  const data = await response.json()
+  render();
+}
+
+const render = () => {
+  const newsHTML = newsList.map(news=>`<div class="row news">
+          <div class="col-lg-4">
+            <img
+              class="news-img-size"
+              src=${news.urlToImage}
+            />
+          </div>
+          <div class="col-lg-8">
+            <h2>${news.title}</h2>
+            <p>${news.description}</p>
+            <div>${news.source.name} ${news.publishedAt}</div>
+          </div>
+        </div>`).join('');
+
+  document.getElementById('news-board').innerHTML=newsHTML
+}
+getLastestNews();
+
+//1. 버튼들에 클릭 이벤트를 넣어줌
+//2. 카테고리별 뉴스 가져오기
+//3. 그 뉴스를 보여주기
